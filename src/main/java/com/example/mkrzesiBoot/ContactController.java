3658f4052106028a75d0b7f3a74b7cc21af55c72
Mkrzesi <maciej.krzesinski@gmail.com>
2021-03-29 00:38:50 +0200

    New code added.

--- src/main/java/com/example/mkrzesiBoot/ContactController.java
+++ src/main/java/com/example/mkrzesiBoot/ContactController.java
@@ -1,4 +1,34 @@
 package com.example.mkrzesiBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
 public class ContactController {
+
+    ContactRepositorium contactRepositorium;
+
+    @Autowired
+    public ContactController(ContactRepositorium contactRepositorium){
+        this.contactRepositorium = contactRepositorium;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Map<String,Object> model){
+        List<Contact> contacts = contactRepositorium.findAll();
+        model.put("contacts", contacts);
+        return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submit(Contact contact){
+        contactRepositorium.save(contact);
+        return "redirect:/";
+    }
 }
