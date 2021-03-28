e52c2c275d9c94677870443dd697585a452cf856
Mkrzesi <maciej.krzesinski@gmail.com>
2021-03-28 15:18:17 +0200

    Clean Spring Boot App.

---
+++ src/main/java/com/example/mkrzesiBoot/MkrzesiBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mkrzesiBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkrzesiBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkrzesiBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mkrzesiBoot/MkrzesiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mkrzesiBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkrzesiBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
